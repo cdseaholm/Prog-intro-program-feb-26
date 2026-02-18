a2329469b65293f326642122ab3fdbb3d1e14db7
Carl Seaholm <cdseaholm@gmail.com>
2026-02-18 14:40:56 -0700

    wednesday commit

--- src/week2/angular-demo/src/app/areas/basics/pages/home.ts
+++ src/week2/angular-demo/src/app/areas/basics/pages/home.ts
@@ -1,16 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-basics-home',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet],
-  template: `
-    <div class="flex flex-col items-center justify-center gap-4">
-      <h2 class="text-2xl font-bold">Basic Stuff You Should Know About Angular</h2>
-    </div>
-    <router-outlet></router-outlet>
-  `,
+  imports: [],
+  template: ` <p>This is the basics stuff!</p> `,
   styles: ``,
 })
 export class HomePage {}
